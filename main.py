7164513ad92e9c2af9a06d417e498bfb94039e7c
BenjaminJacober <jacober.benjamin@gmail.com>
2021-12-03 15:29:42 +0100

    M151 - finished frontend

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import json
+
 import flask
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def page():
-    return flask.render_template("home.html")
+@app.route("/", methods=['GET', 'POST'])
+def homePage():
+    if(flask.request.method == 'GET'):
+        return flask.render_template("home.html")
+    if (flask.request.method == 'POST'):
+        fname = flask.request.form.get('firstname')
+        lname = flask.request.form.get('lastname')
+        bdate = flask.request.form.get('birthdate')
+        mail = flask.request.form.get('email')
+        ahv = flask.request.form.get('ahv')
+        pnumber = flask.request.form.get('pnumber')
+        section = flask.request.form.get('section')
+        job = flask.request.form.get('job')
+        return
+
+@app.route("/getPersons", methods=['GET'])
+def persons():
+    if (flask.request.method == 'GET'):
+        file = open("test_data/persons.json")
+        data = json.load(file)
+        file.close()
+        return flask.jsonify(data)
+
+
 
 if __name__ == '__main__':
     app.run()
