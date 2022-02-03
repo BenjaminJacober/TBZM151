ce5c0fee80a8c767afb9b8cdce9b533f2d061538
BenjaminJacober <jacober.benjamin@gmail.com>
2022-02-03 22:59:24 +0100

    add cross origin and first backend call

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,5 +1,7 @@
 import {Badge, ListGroup, Row} from "react-bootstrap";
 import PageLayout from "./pageLayout/PageLayout";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 const myProducts = [
     {
@@ -11,19 +13,36 @@ const myProducts = [
     }
 ]
 
+const getProducts = async () => {
+    try {
+        return await axios.get('http://localhost:5000/stock');
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+getProducts();
+
 const Stock = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+            getProducts().then(response => setProducts([response.data]))
+    })
+
     return <PageLayout>
         <Row>
             <ListGroup as="ol" numbered>
-            {myProducts.map(product => {
-                return <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">{product.name}</div>
-                        {product.name + ", best before: " + product.bestBeforeDate}
-                    </div>
-                    <Badge variant="primary" pill>{product.amount + " " + product.unitType + "."}</Badge>
-                </ListGroup.Item>
-            })}
+                {products.map(product => {
+                    return <ListGroup.Item key={product.name + product.amount.toString()} as="li" className="d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">{product.name}</div>
+                            {product.name + ", best before: " + product.bestBeforeDate}
+                        </div>
+                        <Badge variant="primary" pill>{product.amount + " " + product.unitType + "."}</Badge>
+                    </ListGroup.Item>
+                })}
             </ListGroup>
         </Row>
     </PageLayout>
